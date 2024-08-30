b1c988998f815b24655fe556daa5215ed2b2ad10
Nathaniel Melendrez <nathaniel.melendrez@infor.com>
2024-08-30 19:33:32 +0800

    Port 8080 update

--- src/index.ts
+++ src/index.ts
@@ -10,13 +10,12 @@ const corsOptions = {
   origin: "*",
   credentials: true,
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  allowedHeaders: "Origin, X-Requested-With, Content-Type, Accept, Authorization",
-} 
+  allowedHeaders:
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+};
 
-app.use(
-  cors(corsOptions)
-);
-app.options("*",cors(corsOptions))
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -24,4 +23,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(`${baseURL}/courses`, courseRoutes);
 // app.use("/", (req, res) => res.send({ app: "web_course" }));
 
-app.listen(process.env.PORT, () => console.log(`Server is listening on port ${process.env.PORT}`));
+app.listen(process.env.PORT || 8080, () =>
+  console.log(`Server is listening on port ${process.env.PORT}`)
+);
